c56df0ee6ba802be65354324664cff417ff3f666
raja-hassan <m.hassan.raj101@gmail.com>
2023-11-20 10:43:43 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let painting = false;
+
+canvas.width = window.innerWidth - 50;
+canvas.height = window.innerHeight / 2;
+
+function startPosition(e) {
+  painting = true;
+  draw(e);
+}
+
+function endPosition() {
+  painting = false;
+  ctx.beginPath();
+}
+
+function draw(e) {
+  if (!painting) return;
+
+  ctx.lineWidth = 5;
+  ctx.lineCap = 'round';
+  ctx.strokeStyle = 'black';
+
+  ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+}
+
+canvas.addEventListener('mousedown', startPosition);
+canvas.addEventListener('mouseup', endPosition);
+canvas.addEventListener('mousemove', draw);
